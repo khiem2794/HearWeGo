8de2433d96b885ccaf2f3e5e929cd81918434eb1
nthhtn <thehien115@gmail.com>
2015-07-17 11:29:36 +0700

    New Update

--- src/HearWeGo/HearWeGoBundle/Controller/CompanyController.php
+++ src/HearWeGo/HearWeGoBundle/Controller/CompanyController.php
@@ -8,6 +8,8 @@ use HearWeGo\HearWeGoBundle\Entity\Company;
 use HearWeGo\HearWeGoBundle\Entity\Tour;
 use HearWeGo\HearWeGoBundle\Form\CompanySignupType;
 use HearWeGo\HearWeGoBundle\Form\CompanySubmitTourType;
+use HearWeGo\HearWeGoBundle\Form\CompanyEditType;
+use HearWeGo\HearWeGoBundle\Form\CompanyDeleteType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -21,9 +23,9 @@ class CompanyController extends Controller
         $company=new Company();
         $form=$this->createForm(new CompanySignupType(),$company,array('method'=>'POST','action'=>$this->generateUrl('company_signup')));
         $form->add('submit','submit');
-        $form->handleRequest($request);
         if ($request->getMethod()=='POST')
         {
+            $form->handleRequest($request);
             if ($form->isValid())
             {
                 $em=$this->getDoctrine()->getEntityManager();
@@ -41,7 +43,6 @@ class CompanyController extends Controller
      */
     public function submitTourAction(Request $request)
     {
-
         if (!$this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
             return $this->redirect($this->generateUrl('user_login'));
         }
@@ -68,5 +69,68 @@ class CompanyController extends Controller
             return new Response('not valid');
         }
         return $this->render('HearWeGoHearWeGoBundle:Company:companySubmit.html.twig',array('form1'=>$form->createView()));
+    }
+
+    /**
+     * @Route("/company/edit",name="company_edit")
+     */
+    public function editCompany(Request $request)
+    {
+        if (!$this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')){
+            return  new Response('Please login');
+        }
+
+        $this->denyAccessUnlessGranted('ROLE_COMPANY', null, 'Unable to access this page!');
+
+        $company=$this->get('security.token_storage')->getToken()->getUser();
+
+        $form=$this->createForm(new CompanyEditType($company),$company,array('method'=>'POST','action'=>$this->generateUrl('company_edit')));
+        $form->add('submit','submit');
+        if ($request->getMethod()=='POST')
+        {
+            $form->handleRequest($request);
+            if ($form->isValid())
+            {
+                $em=$this->getDoctrine()->getEntityManager();
+                $em->persist($company);
+                $em->flush();
+                return $this->render('HearWeGoHearWeGoBundle:Company:companyEdit.html.twig',array('form'=>$form->createView()));
+            }
+        }
+        return $this->render('HearWeGoHearWeGoBundle:Company:companyEdit.html.twig',array('form'=>$form->createView()));
+    }
+
+    /**
+     *@Route("/company/delete",name="company_delete")
+     */
+    public function deleteCompany(Request $request)
+    {
+        if (!$this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')){
+            return  new Response('Please login');
+        }
+
+        $this->denyAccessUnlessGranted('ROLE_COMPANY', null, 'Unable to access this page!');
+
+        $company=$this->get('security.token_storage')->getToken()->getUser();
+
+        $arr=array();
+        $form=$this->createFormBuilder($arr,array('method'=>'DELETE','action'=>$this->generateUrl('company_delete')));
+        $form->add('password','password')->add('delete','submit');
+        if ($request->getMethod()=='DELETE')
+        {
+            $form=$form->getForm();
+            $form->handleRequest($request);
+            echo $form->get('password')->getData()."<br>".$company->getPassword();
+            if ($form->get('password')->getData()==$company->getPassword())
+            {
+                $em=$this->getDoctrine()->getEntityManager();
+                $em->remove($company);
+                $em->flush();
+                return new Response('Deleted!');
+            }
+            else
+                return new Response('Wrong password!');
+        }
+        return $this->render('HearWeGoHearWeGoBundle:Company:companyDelete.html.twig',array('form'=>$form->getForm()->createView()));
     }
 }
---
+++ src/HearWeGo/HearWeGoBundle/Form/CompanyEditType.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace HearWeGo\HearWeGoBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use HearWeGo\HearWeGoBundle\Entity\Company;
+
+class CompanyEditType extends AbstractType
+{
+    protected $company;
+
+    public function __construct(Company $company)
+    {
+        $this->company=$company;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCompany()
+    {
+        return $this->company;
+    }
+
+    /**
+     * @param mixed $company
+     */
+    public function setCompany($company)
+    {
+        $this->company = $company;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name','text',array('data'=>$this->company->getName()))
+            ->add('email','email',array('data'=>$this->company->getEmail()))
+            ->add('phone','text',array('data'=>$this->company->getPhone()))
+            ->add('address','text',array('data'=>$this->company->getAddress()))
+            ->add('password','password')
+            ->add('role', 'hidden', array(
+                'data' => "ROLE_COMPANY"
+            ))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array('data_class'=>"HearWeGo\HearWeGoBundle\Entity\Company"));
+    }
+
+    public function getName()
+    {
+        return 'company_edit';
+    }
+}
+?>
